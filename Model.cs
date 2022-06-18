4e9d62b0c5635bcb04d91ec4cd3430708a0c9d6a
Łukasz Jasionowski <85828127+Lukasz-Jasionowski@users.noreply.github.com>
2022-06-18 08:31:36 +0200

    Fixed adding a new record.

--- Model.cs
+++ Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -7,29 +8,25 @@ using System.Threading.Tasks;
 
 namespace dotNET_Z3
 {
-    class Model : INotifyPropertyChanged
+    class Model
     {
-        LinkedList<Record> records = new LinkedList<Record>();
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public LinkedList<Record> Records { get => records; set => records = value; }
+        public ObservableCollection<Record> Records{ get; set;} = new ObservableCollection<Record>();
 
         public Model()
         {
-            records.AddLast(new Record("Alladyn", "Guy Ritchie", "Disney", "DVD", "02:08:12"));
-            records.AddLast(new Record("Mainstream Sellout", "Machine Gun Kelly", "Bad Boy/Interscope", "CD", "00:40:18"));
-            records.AddLast(new Record("That’s the Spirit", "Bring Me The Horizon", "RCA Records Label", "CD", "00:43:61"));
-            records.AddLast(new Record("Warcraft: Początek", "Duncan Jones", "Legendary Pictures", "DVD", "02:03:05"));
-            records.AddLast(new Record("Solutions ", "K.Flay", "Night Street Records", "CD", "00:36:48"));
-            records.AddLast(new Record("Tomb Raider", "Roar Uthaug", "Warner Bros", "DVD", "01:58:13"));
+            Records.Add(new Record("Alladyn", "Guy Ritchie", "Disney", "DVD", "02:08:12"));
+            Records.Add(new Record("Mainstream Sellout", "Machine Gun Kelly", "Bad Boy/Interscope", "CD", "00:40:18"));
+            Records.Add(new Record("That’s the Spirit", "Bring Me The Horizon", "RCA Records Label", "CD", "00:43:61"));
+            Records.Add(new Record("Warcraft: Początek", "Duncan Jones", "Legendary Pictures", "DVD", "02:03:05"));
+            Records.Add(new Record("Solutions ", "K.Flay", "Night Street Records", "CD", "00:36:48"));
+            Records.Add(new Record("Tomb Raider", "Roar Uthaug", "Warner Bros", "DVD", "01:58:13"));
         }
 
         internal Record AddNewRecord()
         {
             Record newRecord = new Record();
-            records.AddLast(newRecord);
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Records"));
+            Records.Add(newRecord);
             return newRecord;
         }
     }
